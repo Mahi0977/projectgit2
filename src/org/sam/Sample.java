082437a47aacaecaaf5a01e49ed5d262b1d38a07
mahi0977 <mahendranjayavel977@gmail.com>
2025-04-12 14:37:50 +0530

    done2

--- src/org/sam/Sample.java
+++ src/org/sam/Sample.java
@@ -6,5 +6,14 @@ public static void main(String[] args) {
 	
 	int a = 3434;
 	System.out.println(a);
+	
+	
+	String add = "java";
+	System.out.println(add);
+	
+	String test2 = "Test case 2";
+	System.out.println(test2);
+	
+	
 }
 }
